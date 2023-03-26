3c5aecfbc3f9e8a946d671478ed5a9bacdfda0e4
MyunghyunNero <mh1kim0910@gmail.com>
2023-03-26 19:56:19 +0900

    오류 수정

--- src/test/java/com/example/cicd/CiCdApplicationTests.java
+++ src/test/java/com/example/cicd/CiCdApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.cicd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = src.main.java.com.example.cicd.CiCdApplication.class)
 class CiCdApplicationTests {
 
 	@Test
