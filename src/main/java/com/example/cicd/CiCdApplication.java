e8bba45f1485dde8ff957853937426731b3096e3
MyunghyunNero <mh1kim0910@gmail.com>
2023-03-26 19:23:16 +0900

    설정 바꾸기

--- src/main/java/com/example/cicd/CiCdApplication.java
+++ src/main/java/com/example/cicd/CiCdApplication.java
@@ -1,4 +1,4 @@
-package com.example.cicd;
+package src.main.java.com.example.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
