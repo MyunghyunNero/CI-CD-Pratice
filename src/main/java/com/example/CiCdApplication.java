b91c3eb67f79bb67f664111d42273911bde931fa
MyunghyunNero <mh1kim0910@gmail.com>
2023-09-24 02:05:59 +0900

    d

--- src/main/java/com/example/CiCdApplication.java
+++ src/main/java/com/example/CiCdApplication.java
@@ -1,4 +1,4 @@
-package src.main.java.com.example.cicd;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
